7c96be6ff9b779f5dafbbd213ad569ec6f2670b4
呉真 <kuretru@gmail.com>
2017-11-20 15:10:03 +0800

    Finished Web problem 20

--- Web/20.c
+++ Web/20.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main()
 {
-	int i, n, m;//n for student,m for tap
-	int remain, location, time = 1, tap[100], student[10000];
+	int i, n, m, end = 1;//n for student,m for tap
+	int remain, location, time = 0, tap[100], student[10000];
 	scanf("%d %d", &n, &m);
 	for(i = 0; i < n; i++)
 	{
@@ -12,22 +12,29 @@ int main()
 	}
 	location = m;
 	remain = n - m;
-	while(remain > 0)
+	while(end)
 	{
-		printf("remain%d ", remain);
 		for(i = 0; i < m; i++)
 		{
 			if(tap[i] > 0)
 				tap[i]--;
 			if(tap[i] == 0)
 			{
-				tap[i] = student[location++];
-				remain--;
+				if(remain > 0)
+				{
+					tap[i] = student[location++];
+					remain--;
+				}
 			}
-			printf("%d ", tap[i]);
 		}
-		printf("\n");
 		time++;
+		for(i = 0; i < m; i++)
+		{
+			if(tap[i] > 0)
+				break;
+			if(i == m - 1)
+				end = 0;
+		}
 	}
 	printf("%d", time);
 	return 0;
