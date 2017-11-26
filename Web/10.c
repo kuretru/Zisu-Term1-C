1ba12ebd7b8dbf5c5fde8ef7f0c87db9662af137
呉真 <kuretru@gmail.com>
2017-11-26 12:50:54 +0800

    Finished Web problem 10

--- Web/10.c
+++ Web/10.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
-#include <string.h>
 int main()
 {
-	int result[10] = {0};
-	int i, num, cur;
-	scanf("%d", &num);
-	for(i = 1; i <= num; i++)
+	int i;
+	int low, cur, high, count, pow, num;
+	int n, result[10] = {0};
+	scanf("%d", &n);
+	for(int i = 0; i < 10; i++)
 	{
-		cur = i;
-		while(cur > 9)
+		num = n;
+		pow = 1;
+		count = 0;
+		while(num / pow != 0)
 		{
-			result[cur % 10]++;
-			cur /= 10;
+			low = num - (num / pow) * pow;
+			cur = (num / pow) % 10;
+			high = num / (pow * 10);
+			if(cur < i)
+				count += high * pow;
+			else if(cur == i)
+				count += high * pow + low + 1;
+			else if(cur > i)
+				count += (high + 1) * pow;
+			if(i == 0)
+				count -= pow;
+			pow *= 10;
 		}
-		result[cur % 10]++;
+		result[i] += count;
 	}
 	for(i = 0; i < 10; i++)
 		printf("%d\n", result[i]);
