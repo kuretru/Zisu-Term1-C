a22f6bdafe7cf0d4712d2f0a74ebcc75ae7c7c5d
呉真 <kuretru@gmail.com>
2017-12-02 19:04:05 +0800

    Finished Web problem 25

--- Web/25.c
+++ Web/25.c
@@ -1,32 +1,68 @@
 #include <stdio.h>
+
+void quickSort(int data[], int left, int right);
+
 int main()
 {
-	int n, m, i, j, count[10], result[100], num, max;
+	int i, j, k;
+	int n, m;
+	int last, count;
+	int oneThird, max[2];
+	int result[100], data[100000];//change to 1000000
 	scanf("%d", &n);
 	for(i = 0; i < n; i++)
 	{
-		for(j = 0; j < 10; j++)
-			count[j] = 0;
 		scanf("%d", &m);
 		for(j = 0; j < m; j++)
+			scanf("%d", &data[j]);
+		quickSort(data, 0, m - 1);
+		oneThird = m / 3;
+		max[0] = max[1] = 0;
+		last = data[0];
+		count = 1;
+		k = 0;
+		for(j = 1; j < m; j++)
 		{
-			scanf("%d", &num);
-			count[num]++;
-		}
-		max = count[0];
-		result[i] = 0;
-		for(j = 1; j < 10; j++)
-		{
-			if(count[j] > max)
+			if(data[j] == last)
 			{
-				max = count[j];
-				result[i] = j;
+				count++;
+				if(j == m - 1 && count > oneThird)
+					max[k++] = last;
+			}
+			else
+			{
+				if(count > oneThird)
+					max[k++] = last;
+				last = data[j];
+				count = 1;
 			}
 		}
+		if(max[1] > max[0])
+			result[i] = max[1];
+		else
+			result[i] = max[0];
 	}
 	for(i = 0; i < n; i++)
-	{
 		printf("%d\n", result[i]);
+}
+
+void quickSort(int data[], int left, int right)
+{
+	if(left >= right)
+		return;
+	int i = left;
+	int j = right;
+	int key = data[left];
+	while(i < j)
+	{
+		while(i < j && key <= data[j])
+			j--;
+		data[i] = data[j];
+		while(i < j && key >= data[i])
+			i++;
+		data[j] = data[i];
 	}
-	return 0;
+	data[i] = key;
+	quickSort(data, left, i - 1);
+	quickSort(data, i + 1, right);
 }
