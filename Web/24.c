381f646f40129f6f01968f4f5960c6bcd6156e88
呉真 <kuretru@gmail.com>
2017-12-09 14:41:38 +0800

    Finished Web problem 24

--- Web/24.c
+++ Web/24.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
 	int i, j, k, groups;
-	float a, b, c, d;
-	float result[20][3], x, fx;
+	double a, b, c, d;
+	double result[20][3], x, fx, last;
 	scanf("%d", &groups);
 	for(i = 0; i < groups; i++)
 	{
-		scanf("%f %f %f %f", &a, &b, &c, &d);
+		scanf("%lf %lf %lf %lf", &a, &b, &c, &d);
 		k = 0;
+		last = -11000;
 		for(j = -10000; j <= 10000; j++)
 		{
 			x = j * 1.00 / 100;
 			fx = a * x * x * x + b * x * x  + c * x  + d;
-			if(fx == 0)
+			if(fabs(fx) <= 0.00000001 && fx - last >= 1)
 				result[i][k++] = x;
 			if(k > 2)
 				break;
 		}
 	}
 	for(i = 0; i < groups; i++)
-		printf("%.2f %.2f %.2f\n", result[i][0], result[i][1], result[i][2]);
+		printf("%.2lf %.2lf %.2lf\n", result[i][0], result[i][1], result[i][2]);
 	return 0;
 }
