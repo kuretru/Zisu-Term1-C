c1142785ff160a6b293a7b0e1f74b02da26178a2
呉真 <kuretru@gmail.com>
2017-12-11 13:43:40 +0800

    Changed sort mothod

--- Web/28.c
+++ Web/28.c
@@ -1,80 +1,79 @@
 #include <stdio.h>
 
+int cmp(int x, int y);
+int data[600][4];
+
 int main()
 {
-	int i, j, k, l;
+	int i, j, k;
 	int m, n;
 	int a, b, c;
 	int x, y, z;
-	int condition, max;
-	int data[600][4];
+	int max;
 	scanf("%d", &m);
-	for(i = 0; i < m; i++)
+	for (i = 0; i < m; i++)
 	{
 		scanf("%d", &n);
-		for(j = 0; j < n; j++)
+		for (j = 0; j < n; j++)
 		{
 			scanf("%d %d %d", &a, &b, &c);
-			k = j * 6;//列出所有组合
-			data[k][0] = a; data[k][1] = b; data[k][2] = c; data[k++][3] = 0;
+			k = j * 6; //列出所有组合
+			data[k][0] = a;	data[k][1] = b;	data[k][2] = c;	data[k++][3] = 0;
 			data[k][0] = a; data[k][1] = c; data[k][2] = b; data[k++][3] = 0;
-			data[k][0] = b; data[k][1] = a; data[k][2] = c; data[k++][3] = 0;
-			data[k][0] = b; data[k][1] = c; data[k][2] = a; data[k++][3] = 0;
-			data[k][0] = c; data[k][1] = a; data[k][2] = b; data[k++][3] = 0;
-			data[k][0] = c; data[k][1] = b; data[k][2] = a; data[k++][3] = 0;
+			data[k][0] = b;	data[k][1] = a;	data[k][2] = c;	data[k++][3] = 0;
+			data[k][0] = b;	data[k][1] = c;	data[k][2] = a;	data[k++][3] = 0;
+			data[k][0] = c;	data[k][1] = a;	data[k][2] = b;	data[k++][3] = 0;
+			data[k][0] = c;	data[k][1] = b;	data[k][2] = a;	data[k++][3] = 0;
 		}
-		for(l = 0; l < 3; l++)//从小到大排序
+		for (j = 0; j < n * 6 - 1; j++)
 		{
-			for(j = 0; j < n * 6 - 1; j++)
+			for (k = 0; k < n * 6 - j - 1; k++)
 			{
-				for(k = 0; k < n * 6 - j - 1; k++)
+				if (cmp(k, k + 1))
 				{
-					condition = 0;
-					switch(l)
-					{
-					case 0:
-						if(data[k][0] > data[k + 1][0])
-							condition = 1;
-						break;
-					case 1:
-						if(data[k][0] == data[k + 1][0] && data[k][1] > data[k + 1][1])
-							condition = 1;
-						break;
-					case 2:
-						if(data[k][0] == data[k + 1][0] && data[k][1] == data[k + 1][1] && data[k][2] > data[k + 1][2])
-							condition = 2;
-						break;
-					}
-					if(condition)
-					{
-						x = data[k][0]; y = data[k][1]; z = data[k][2];
-						data[k][0] = data[k + 1][0];
-						data[k][1] = data[k + 1][1];
-						data[k][2] = data[k + 1][2];
-						data[k + 1][0] = x; data[k + 1][1] = y; data[k + 1][2] = z;
-					}
+					x = data[k][0];	y = data[k][1];	z = data[k][2];
+					data[k][0] = data[k + 1][0];data[k][1] = data[k + 1][1];data[k][2] = data[k + 1][2];
+					data[k + 1][0] = x;	data[k + 1][1] = y;	data[k + 1][2] = z;
 				}
 			}
 		}
-		for(j = 0; j < n * 6; j++)//最长不下降序列
+		for (j = 0; j < n * 6; j++) //最长不下降序列
 		{
 			data[j][3] = data[j][2];
-			for(k = 0; k < j; k++)
+			for (k = 0; k < j; k++)
 			{
-				if(data[j][0] > data[k][0] && data[j][1] > data[k][1])
+				if (data[j][0] > data[k][0] && data[j][1] > data[k][1])
 				{
-					if(data[k][3] + data[j][2] > data[j][3])
+					if (data[k][3] + data[j][2] > data[j][3])
 						data[j][3] = data[k][3] + data[j][2];
 				}
 			}
 		}
 		max = data[0][3];
-		for(j = 1; j < n * 6; j++)
+		for (j = 1; j < n * 6; j++)
 		{
-			if(data[j][3] > max)
+			if (data[j][3] > max)
 				max = data[j][3];
 		}
 		printf("%d\n", max);
 	}
 	return 0;
+}
+
+int cmp(int x, int y)
+{
+	int condition = 0;
+	if (data[x][0] > data[y][0])
+		condition = 1;
+	else if (data[x][0] == data[y][0])
+	{
+		if (data[x][1] > data[y][1])
+			condition = 1;
+		else if (data[x][1] == data[y][1])
+		{
+			if (data[x][2] > data[y][2])
+				condition = 1;
+		}
+	}
+	return condition;
 }
